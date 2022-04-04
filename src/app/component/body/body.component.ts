dc60902a12919dcdfc6d925d5dfec4c6341e5961
Juan Manuel <95294025+jmbross@users.noreply.github.com>
2022-04-03 23:52:58 -0300

    Pruebas

--- src/app/component/body/body.component.ts
+++ src/app/component/body/body.component.ts
@@ -1,4 +1,5 @@
 import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { Data } from '@angular/router';
 import { Image } from 'src/app/Image';
 import { ImagesService } from 'src/app/service/images.service';
 
@@ -18,6 +19,7 @@ export class BodyComponent implements OnInit {
   modal: any;
 
 
+
   @Output() btnClick = new EventEmitter();
 
   constructor(private imagesService: ImagesService) { }
@@ -25,11 +27,19 @@ export class BodyComponent implements OnInit {
   ngOnInit(): void {
     this.imagesService.getImages().subscribe(data => {
       this.data = data;
-      console.log(this.data)
+
+      console.log(this.data);
     })
   }
   onbtnClick(Image:any) {
     this.title = (Image.title);
     this.url = (Image.url);
   }
+
+pepito(Image:any){
+  this.title = (Image.title)
+  console.log(this.title)
+}
+
+
 }
